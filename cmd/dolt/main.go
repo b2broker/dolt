a91128422d99008c5c4536ce3de672a37c787763
Maxim P. Kalenich <maxim.kalenich@gmail.com>
2021-12-06 19:00:21 +0000

    feat: :sparkles: Declare base flow, with todo's and healthServer
    MTW-1460 #time 30m declare base app flow

--- cmd/dolt/main.go
+++ cmd/dolt/main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+type Handler struct{}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Hello %#v", *r)
+	w.Write([]byte("OK"))
+}
+
 func main() {
+	// TODO: Read configuration from environment
+
+	sigint := make(chan os.Signal, 1)
+
+	// TODO: Define list of ignored syscall Signals
+	// signal.Ignore(syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGKILL)
+
+	signal.Notify(sigint)
+
+	healthSrv := http.Server{
+		Addr:    ":8080",
+		Handler: &Handler{},
+	}
+
+	go func() {
+		<-sigint
+		// TODO: Postpone server shutdown regarding Env variable
+		if err := healthSrv.Shutdown(context.Background()); err != nil {
+			log.Printf("HTTP server Shutdown with error: %v", err)
+		}
+	}()
+
+	// TODO: Postpone server start regarding Env variable
+
+	log.Printf("Server has been started and listen on: %s", healthSrv.Addr)
+	if err := healthSrv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+
+	// TODO: Suden interrupt with non-0 exit code
+	log.Println("Server has been stopped")
 }
